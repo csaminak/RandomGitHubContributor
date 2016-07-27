36c84e48d87f8b860d48f795995b1094802c4d81
Samina Chhatwal <csaminak@gmail.com>
2016-07-27 16:41:13 -0400

    updated function definitions and what they do

--- src/js/app.js
+++ src/js/app.js
@@ -2,52 +2,70 @@
     'use strict';
     window.app = ns = (ns || {});
 
-    var token;
 
     $('#search').on('submit', function retrieveRepositories(event){
         event.preventDefault();
-        if (!token) {
-            token = $('#api-key').val();
-        }
-        localStorage.setItem('token', token);
-
+        var token = $('#api-key').val();
         var searchQuery = $('input[value="Find Contributor"]').val();
 
-        findContributor(token, searchQuery)
-            .done(selectRandomData)
+        getRepos(token, searchQuery)
+            .done(function(data) {
+                selectRandomRepo(data);
+            })
             .fail(function(xhr){
                 console.log(xhr);
             });
     });
 
 
+
+
+
+    // function getCommits(repo) {
+    //     var name = repo.full_name.split('/');
+    //     name = name[0];
+    //     console.log(name);
+    //     return $.ajax({
+    //         url: 'https://api.github.com/repos/' + name + '/' + repo.name + '/commits',
+    //         method: 'get',
+    //         headers: {'Authorization': 'token ' + token},
+    //         dataType: 'json'
+    //     });
+    // }
+
+
     /**
-     * Takes the data object and searches the array in that data to select
-     * a random Object within that array to display.
-     * @param  {Object}    data    a list of repositories matching the search
-     * @return {Object}            a random individual repository and it's info
+     * Takes the data object and finds items which holds the array of repos
+     * and then selects a random object/repo within that array to display.
+     * @param  {Object}    data   a list of repositories matching the search
+     * @return {Promise}          a random individual repo object and it's info
      */
-    function selectRandomData(data) {
-        var dataList = data.items;
-        console.log(dataList);
-        var i = Math.ceil(Math.random() * (dataList.length - 1));
-        console.log(dataList[i]);
-        return dataList[i];
+    function selectRandomRepo(data) {
+        return new Promise(function(resolve, reject) {
+            if(!data || data.items) {
+                return reject(new Error('Ajax call did not retrieve repos'));
+            }
+            var dataList = data.items;
+            console.log(dataList);
+            var repo = dataList[Math.floor(Math.random() * (dataList.length))];
+            console.log(repo);
+            resolve(repo);
+        });
     }
 
 
     /**
-     * Takes the token and searchQuery from user and send a request to GitHub
+     * Takes the token and searchQuery from user and sends a request to GitHub
      * to retrieve all the repositories that match the search.
-     * @param  {String}     token           the user's secure apiKey
+     * @param  {String}     apiKey           the user's secure apiKey
      * @param  {String}     searchQuery     the repository search term(s)
-     * @return {jQuery xhr Object}          the promises returned
+     * @return {jQuery xhr Object}          holds promises that can be implemented
      */
-    function findContributor(token, searchQuery){
+    function getRepos(apiKey, searchQuery){
         return $.ajax({
             url: 'https://api.github.com/search/repositories?q=' + searchQuery,
             mehtod: 'get',
-            headers: {'Authorization': 'token ' + token},
+            headers: {'Authorization': 'token ' + apiKey},
             dataType: 'json'
         });
     }
